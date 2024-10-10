26533c3c7d31338e85cba6824b30a44faaa33b8c
1e37 <dbz.gaming@outlook.de>
2024-10-10 19:51:20 +0200

    Update value

--- Programming Concepts/3.Operators.php
+++ Programming Concepts/3.Operators.php
@@ -1,9 +1,11 @@
 <?php 
-
+echo "-----------------------------------------\n";
 echo "37 + 13 = " . addition(37,13) ."\n";
-echo "13 - 37 = " . substraction(1,1) ."\n";
+echo "13 - 37 = " . substraction(13, 37) . "\n";
 echo "7 * 4 = " . multiplication(7,4) ."\n";
-echo "24 / 7 = " . division(24,7) ."\n\n";
+echo "24 / 7 = " . division(24,7) ."\n";
+echo "1 ^ 10 = " . power(1,10) ."\n\n";
+echo "-----------------------------------------\n";
 echo "My username as number : " . _1e(37) . "\n";
 
 function addition($a, $b){
@@ -24,12 +26,12 @@ function division($a, $b){
 function power($base, $exponent){
     // Check input
     if (!is_int($exponent) || $exponent < 0) {
-        throw new Exception("Exponent must be a non-negative integer");
+        throw new Exception("Exponent must be positive integer");
     }
 
     // non null value
     $result = 1;
-    // for each iteration {i} aslong {exponent} is //higger then {i}.
+    // do for {i=0} each iteration as {i} aslong {exponent} is greater then {i} times.
     for ($i = 0; $i < $exponent; $i++) {
         $result = multiplication($result, $base);
     }
